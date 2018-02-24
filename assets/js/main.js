805d3e0b0d8c42a04595c28b193b94cba98ae5df
Benny Hwang <bennyhwangg@gmail.com>
2018-02-25 10:50:40 +1100

    image gallery added and unused image removed

--- assets/js/main.js
+++ assets/js/main.js
@@ -118,3 +118,34 @@
 
 })(jQuery);
 
+
+// // Script to trigger lightbox
+// $('.lightbox_trigger').click(function(e) {
+// 	e.preventDefault();
+// 	var image_href = $(this).attr("href");
+// 	if ($('#lightbox').length > 0) { // #lightbox exists
+	
+// 		//insert img tag with clicked link's href as src value
+// 		$('#content').html('<img src="' + image_href + '" />');
+	   	
+// 		//show lightbox window - you can use a transition here if you want, i.e. .show('fast')
+// 		$('#lightbox').show();
+// 	} else { //#lightbox does not exist 
+	
+// 		//create HTML markup for lightbox window
+// 		var lightbox = 
+// 		'<div id="lightbox">' +
+// 			'<p>Click to close</p>' +
+// 			'<div id="content">' + //insert clicked link's href into img src
+// 				'<img src="' + image_href +'" />' +
+// 			'</div>' +	
+// 		'</div>';
+			
+// 		//insert lightbox HTML into page
+// 		$('body').append(lightbox);
+// 	}
+// });
+
+// $('#lightbox').on('click', 'a', function() {
+// 	$('#lightbox').hide();
+// });
